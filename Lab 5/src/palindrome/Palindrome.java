30261823f376bcd2a1db7b41dcfdd561e7c1b4a4
HannahJ <hanjs01@gmail.com>
2017-09-26 13:28:45 -0500

    finished isaPalindrome method

---
+++ Lab 5/src/palindrome/Palindrome.java
@@ -0,0 +1,37 @@
+package palindrome;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String s = input.nextLine();
+		input.close();
+
+		if(isaPalindrome(s))
+			System.out.print("This is a palindrome");
+		else
+			System.out.print("This is not a palindrome");
+
+	}
+	
+	public static boolean isaPalindrome(String str)
+	{	
+		
+		if(str.length()==0)
+			return false;
+		
+		if(str.length()==1)
+			return false;
+		
+		if(str.charAt(0)==(str.charAt(str.length()-1)))
+			isaPalindrome(str.substring(1, str.length()-1));
+		else
+			return false;
+		
+	return true;
+	}//end method
+
+}//end class
